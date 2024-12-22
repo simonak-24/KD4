5f1bf4ffa43209096618674c5105faaff73612a8
simonak-24 <simonchiks@gmail.com>
2024-12-22 13:44:55 +0200

    initial files

---
+++ sk23074_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from sk23074_libadd import *
+from sk23074_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
